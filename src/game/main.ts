2d1b9ccc2bd73a11078c30c97d128ac052edf6f2
anhnt <tien226anh@gmail.com>
2025-05-15 21:58:28 +0700

    small refactor

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,5 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -13,12 +9,15 @@ const config: Phaser.Types.Core.GameConfig = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: false,
+        }
+    },
     scene: [
-        Boot,
-        Preloader,
-        MainMenu,
         MainGame,
-        GameOver
     ]
 };
 
